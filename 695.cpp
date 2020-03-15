957f4ec6afe85b591a0fe343010336038afb1814
happy-luck <18813129242@163.com>
2020-03-15 18:59:02 +0800

    深度优先搜索

--- 695.cpp
+++ 695.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 class Solution {
-    int dfs(vector<vector<int>>& grid, int cur_i, int cur_j) {
+    int dfs(vector<vector<int> >& grid, int cur_i, int cur_j) {
         if (cur_i < 0 || cur_j < 0 || cur_i == grid.size() || cur_j == grid[0].size() || grid[cur_i][cur_j] != 1)
             return 0;
         grid[cur_i][cur_j] = 0;
@@ -19,7 +19,7 @@ class Solution {
         return ans;
     }
 public:
-    int maxAreaOfIsland(vector<vector<int>>& grid) {
+    int maxAreaOfIsland(vector<vector<int> >& grid) {
         int ans = 0;
         for (int i = 0; i != grid.size(); ++i)
             for (int j = 0; j != grid[0].size(); ++j)
@@ -30,5 +30,39 @@ public:
 
 int main()
 {
-
+    
+        //正确的插入方式
+    vector<vector<int> > A;
+    //A.push_back里必须是vector
+    vector<int> B;
+    B.push_back(1);
+    B.push_back(1);
+    B.push_back(0);
+    B.push_back(0);
+    B.push_back(0);
+    A.push_back(B);
+    B.clear();
+    B.push_back(1);
+    B.push_back(1);
+    B.push_back(0);
+    B.push_back(0);
+    B.push_back(0);
+    A.push_back(B);
+    B.clear();
+    B.push_back(0);
+    B.push_back(0);
+    B.push_back(0);
+    B.push_back(1);
+    B.push_back(1);
+    A.push_back(B);
+    B.clear();
+    B.push_back(0);
+    B.push_back(0);
+    B.push_back(0);
+    B.push_back(1);
+    B.push_back(1);
+    A.push_back(B);
+    Solution S;
+    int ans = S.maxAreaOfIsland(A);
+    printf("%d\n", ans);
 };
