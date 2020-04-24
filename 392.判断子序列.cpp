4e1e78ffe04a0c3b88001326fffef4c23e934a0a
happy-luck <18813129242@163.com>
2020-04-24 08:23:57 +0800

    easy

---
+++ 392.判断子序列.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        if (s==""){
+            return true;
+        }
+        int j=0;
+        for(int i=0;i<t.length();i++){
+            if(t[i]==s[j]){
+                j++;
+            }
+            if(j==s.length()){
+                return true;
+            }
+        }
+        return false;
+    }
+};
