c7658843c0a8d17a549901a0c45b790b737aafa2
happy-luck <18813129242@163.com>
2020-05-12 16:40:37 +0800

    贪心

---
+++ 1338.数组大小减半.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        unordered_map<int, int> freq;
+        for (int num: arr) {
+            ++freq[num];
+        }
+        vector<int> occ;
+        for (auto& [k, v]: freq) {
+            occ.push_back(v);
+        }
+        sort(occ.begin(), occ.end(), greater<int>());
+        int cnt = 0, ans = 0;
+        for (int c: occ) {
+            cnt += c;
+            ans += 1;
+            if (cnt * 2 >= arr.size()) {
+                break;
+            }
+        }
+        return ans;
+    }
+};
